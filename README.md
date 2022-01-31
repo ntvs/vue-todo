7ef8944a88c749c9a9539420e33ec7e70f9c9e09
Nick <NickRibs@hotmail.com>
2022-01-31 15:31:51 -0500

    Fixed deletion error again by implementing deletion by object ID attribute - detailed in README.md

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # "Anime list" / TODO test app
 ## Implemented using Vue.js 3
----
 
 **Features:**
 - Add title, picture
@@ -10,7 +9,12 @@
 - CSS
 
 **Problems:**
+1. No form sanitation
+2. Import { ref } from vue VS. direct editing with v-model
+3. Categories hard 
+
+**Resolved**
 1. Deleting anime through favorites array does not delete the corresponding anime in the anime array
-2. No form sanitation
-3. Import { ref } from vue VS. direct editing with v-model
-4. Categories hard coded
+    - This happened because the position of the object in the favorites array was not the same as the position of the object in the original animes array.
+        - An incorrect solution to this was committed by adding an attribute which listed each anime's original index in the animes array. This was an incorrect solution because if an object before it in the array was deleted, the indices of each object after the deleted object would change and the index stored in the index attribute of each object in the animes array would be incorrect. So, using the index stored in each object as an attribute to delete objects resulted in the incorrect object being deleted once again or nothing being deleted at all since no object would be at the specified index.
+    - This was resolved by passing in the object's unique ID into the deletion method. Then, using the Array.prototype.findIndex() method to iterate through the objects of the original animes array, the index of the object which had an ID strictly equal to the ID passed into the deletion method could be found. Now, whatever the position and whatever the array, a deletion by ID could be performed.
--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = Vue.createApp({
         return {
             melee: "1.02",
             animes: [
-                { id: 1, index: 0, title: "Death Note", image: "https://qph.fs.quoracdn.net/main-qimg-f6b7ddfdc6338864813aba63c6af255c", isFav: false },
-                { id: 2, index: 1, title: "One Piece", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVWcYD_1oq1Q9itvYBMW0-3DFqpA8dhXrM4ByuAxtC0xzFokr4eUAa2E8pXrYpUTj5p_I&usqp=CAU", isFav: false },
-                { id: 3, index: 2, title: "My Hero", image: "https://i1.sndcdn.com/avatars-000410733648-9kv78w-t500x500.jpg", isFav: false },
-                { id: 4, index: 3, title: "Dragon Ball Z", image: "https://pbs.twimg.com/profile_images/1366466342354751491/JyhZpbtu_400x400.jpg" },
-                { id: 5, index: 4, title: "CODE GEASS", image: "https://i1.sndcdn.com/avatars-000151838815-1kli9j-t500x500.jpg" },
-                { id: 6, index: 5, title: "MELEE", image: "https://commondatastorage.googleapis.com/images.pricecharting.com/AMIfv94OrwfVaIwl-NvTFTeRhzeNT5M2ZMjj9gwBwXBcmT5kntbY0sdQZrq0mv3nYE2WoBeUQc511bnQNS11D7-iEydu33azzeK5Rm7MPeIjRpkpBUXuTXDOz4vruA2D8ddAK_B9PmTNttt5pakDvyDgaj7ymtAxIQ/240.jpg"}
+                { id: 1, title: "Death Note", image: "https://qph.fs.quoracdn.net/main-qimg-f6b7ddfdc6338864813aba63c6af255c", isFav: false },
+                { id: 2, title: "One Piece", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVWcYD_1oq1Q9itvYBMW0-3DFqpA8dhXrM4ByuAxtC0xzFokr4eUAa2E8pXrYpUTj5p_I&usqp=CAU", isFav: false },
+                { id: 3, title: "My Hero", image: "https://i1.sndcdn.com/avatars-000410733648-9kv78w-t500x500.jpg", isFav: false },
+                { id: 4, title: "Dragon Ball Z", image: "https://pbs.twimg.com/profile_images/1366466342354751491/JyhZpbtu_400x400.jpg" },
+                { id: 5, title: "CODE GEASS", image: "https://i1.sndcdn.com/avatars-000151838815-1kli9j-t500x500.jpg" },
+                { id: 6, title: "MELEE", image: "https://commondatastorage.googleapis.com/images.pricecharting.com/AMIfv94OrwfVaIwl-NvTFTeRhzeNT5M2ZMjj9gwBwXBcmT5kntbY0sdQZrq0mv3nYE2WoBeUQc511bnQNS11D7-iEydu33azzeK5Rm7MPeIjRpkpBUXuTXDOz4vruA2D8ddAK_B9PmTNttt5pakDvyDgaj7ymtAxIQ/240.jpg"}
             ],
             animeName: "",
             animeURL: "",
@@ -31,7 +31,6 @@ const app = Vue.createApp({
             if (this.animeName !== "" && this.animeURL !== "") {
                 this.animes.push({
                     id: Date.now(),
-                    index: this.animes.length, //Keeps track of the index of the anime's original position
                     title: this.animeName,
                     image: this.animeURL,
                     isFav: false
@@ -41,8 +40,18 @@ const app = Vue.createApp({
                 this.animeURL = ""
             }
         },
-        deleteAnime(index) {
-            this.animes.splice(index, 1)
+        deleteAnime(animeID) {
+            //If you do the code below, it will only be true if 
+            //the references to the objects are the same
+            //And so if you have two arrays containing that object even though their properties are the same
+            //They may not be refering to the same exact object
+            //let foundIndex = this.animes.findIndex(object => object === anime)
+            
+            //This will compare the properties of the object in the original array to the
+            //Property value passed into the deleteAnime method
+            let foundIndex = this.animes.findIndex(object => object.id === animeID)
+            
+            this.animes.splice(foundIndex, 1)
         }
 
     },
